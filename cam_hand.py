c1837f9f6acc8b4b1fd8439e1959ade9a007ba67
dae4 <3210m0123@gmail.com>
2021-01-31 00:16:58 +0900

    Test Hands

---
+++ cam_hand.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# For static images:
+hands = mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5)
+# For webcam input:
+hands = mp_hands.Hands(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+  success, image = cap.read()
+  if not success:
+    print("Ignoring empty camera frame.")
+    # If loading a video, use 'break' instead of 'continue'.
+    continue
+
+  # Flip the image horizontally for a later selfie-view display, and convert
+  # the BGR image to RGB.
+  image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+  image.flags.writeable = False
+  results = hands.process(image)
+
+  # Draw the hand annotations on the image.
+  image.flags.writeable = True
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  if results.multi_hand_landmarks:
+    for hand_landmarks in results.multi_hand_landmarks:
+      mp_drawing.draw_landmarks(
+          image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+  cv2.imshow('MediaPipe Hands', image)
+  if cv2.waitKey(5) & 0xFF == 27:
+    break
+hands.close()
+cap.release()
